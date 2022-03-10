377ab7d8733cb71dd1e1b6fb474b70b78851b44e
mananjay19 <54362552+mananjay19@users.noreply.github.com>
2022-03-10 17:28:07 +0530

    Add files via upload

--- p128.py
+++ p128.py
@@ -6,9 +6,9 @@ url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
 page = requests.get(url)
 soup = bs(page.text,'html.parser')
 star_table = soup.find_all('table')
-temp_list= []
-table_rows = star_table[8].find_all('tr')
 
+temp_list= []
+table_rows = star_table[5].find_all('tr')
 for tr in table_rows:
     td = tr.find_all('td')
     row = [i.text.rstrip() for i in td]
@@ -28,5 +28,4 @@ for i in range(1,len(temp_list)):
 
 df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius,)),columns=['Star_name','Distance','Mass','Radius'])
 print(df2)
-
 df2.to_csv('dwarf_stars.csv')
